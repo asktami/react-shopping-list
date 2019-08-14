1f18c554d520e6ee01cdfe7b5f0c14976effb66e
Tami Williams <tami@asktami.com>
2019-08-14 16:03:43 -0400

    Updates

---
+++ precache-manifest.518430cce1e797cd9a06ab8e019bf371.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16f0844165335dc86d5aaec27edc0d1",
+    "url": "/react-shopping-list/index.html"
+  },
+  {
+    "revision": "0843722895a1cf9afd12",
+    "url": "/react-shopping-list/static/css/main.ecc54d3b.chunk.css"
+  },
+  {
+    "revision": "7191d2ef8806bace09b7",
+    "url": "/react-shopping-list/static/js/2.8425e0a6.chunk.js"
+  },
+  {
+    "revision": "0843722895a1cf9afd12",
+    "url": "/react-shopping-list/static/js/main.1f0a67a3.chunk.js"
+  },
+  {
+    "revision": "236a0bc9dc996bfb37d4",
+    "url": "/react-shopping-list/static/js/runtime~main.21d58f5c.js"
+  }
+]);
